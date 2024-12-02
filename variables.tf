fb80707a623ee5eaec5700dd91646cfbac2e38e4
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-12-02 13:56:09 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,11 @@ variable "morpheus_url" {
   description = "The URL of the Morpheus Data Appliance where requests will be directed."
 }  
 
-# variable "morpheus_access_token" {
-#   type        = string
-#   description = "Access Token of Morpheus user. This can be used instead of authenticating with Username and Password."
-# }
+variable "morpheus_access_token" {
+  type        = string
+  description = "Access Token of Morpheus user. This can be used instead of authenticating with Username and Password."
+}
+
 variable "tenant_subdomain" {
   type        = string
   description = "Subtenant ID name"
